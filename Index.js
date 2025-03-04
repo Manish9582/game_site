2cc961a7a0c4152c7620543a867c7ae60ed345c3
Manish_Kumar <mk5758029@gmail.com>
2025-03-04 18:38:15 +0530

    add css and js some modification

--- Index.js
+++ Index.js
@@ -295,7 +295,7 @@ const navBar = document.querySelector('.NavbarCon');
 
 // Listen for the scroll event
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) {
+    if (window.scrollY > 60) {
         navBar.classList.add('scrolled');
     } else {
         navBar.classList.remove('scrolled');
